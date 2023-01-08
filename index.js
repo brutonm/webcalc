de2c1cc0eca16c6cabdd4591b5e6b838c4be7c29
Mike Bruton <Shannon_725@hotmail.com>
2023-01-08 18:42:17 -0500

    geting some progress done

---
+++ index.js
@@ -0,0 +1,10 @@
+let runningTotal = 0;
+let operand1 = 0;
+let operand2 = 0;
+let whatMathAreWeDoing = '';
+let doingSomeMath = false;
+let resetInput = false;
+
+document.addEventListener('DOMContentLoaded', e => {
+	
+});
